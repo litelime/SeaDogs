961a0f5138b51797977a46c2ffd5fc92e5c865ec
Jason Joliet <jjoliet@asu.edu>
2018-07-06 11:40:19 -0700

    Deleting File that is incorporated in SP_Locations

--- StoredProcedures/SP_INSERT_LOCATION.sql
+++
@@ -1,7 +0,0 @@
-create or replace PROCEDURE "SP_INSERT_LOCATION" (loc_id varchar, user_id varchar, street varchar, city varchar, state_name varchar, country varchar, zip varchar)
-as
-begin
-  insert into locations(location_id, user_id, street, city, state, country, zip)
-  values(loc_id, user_id, street, city, state_name, country, zip);
-
-end;
