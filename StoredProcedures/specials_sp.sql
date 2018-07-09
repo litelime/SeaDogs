e3e6db7b8cbb3dcd1530cec7e9b70d52dae7e331
syntel <syntel@PHX440G3-28161G.syntelorg.com>
2018-07-09 11:09:58 -0700

    fix some in specials

--- StoredProcedures/specials_sp.sql
+++ StoredProcedures/specials_sp.sql
@@ -1,7 +1,7 @@
-create or replace PROCEDURE "SP_INSERT_SPECIAL" (special_id varchar, special_name varchar, special_description varchar, item_id varchar, discount_percentage number, new_price number)
+create or replace PROCEDURE "SP_INSERT_SPECIAL" (special_id varchar, special_name varchar, special_description varchar, item_id varchar, discount_percentage number, new_price number, photo varchar, veg varchar)
 as
 begin
-  insert into specials(special_id, special_name, special_description, item_id, discount_percentage, new_price)
-  values(special_id, special_name, special_description, item_id, discount_percentage, new_price);
+  insert into specials(special_id, special_name, special_description, item_id, discount_percentage, new_price, photo, veg)
+  values(special_id, special_name, special_description, item_id, discount_percentage, new_price, photo, veg);
 end;
 /
