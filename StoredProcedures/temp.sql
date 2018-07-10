8f9b172ed942fed575cb629b454c6e6fad0646b8
syntel <syntel@PHX440G3-28161G.syntelorg.com>
2018-07-10 10:24:40 -0700

    changed some in specials

--- StoredProcedures/temp.sql
+++ StoredProcedures/temp.sql
@@ -2,11 +2,13 @@ ALTER TABLE specials
   ADD (special_id number,
   special_name varchar(4000),
   special_description varchar(4000),
-  new_price number,
+  amount number,
   photo varchar(4000),
   veg varchar(1));
   /
-  UPDATE specials SET special_id = 0 WHERE special_id IS NULL;
-  /
-  UPDATE specials SET special_name = 'good meal' WHERE special_id = 0;
-  /
+  
+  --UPDATE specials SET special_id = 0 WHERE special_id IS NULL
+  --ALTER TABLE specials ALTER COLUMN special_id VARCHAR NOT NULL
+  --/
+  --UPDATE specials SET special_name = 'good meal' WHERE special_id = 0;
+  --/
