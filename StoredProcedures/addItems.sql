6551157a2b44c4dd22f8b795097156d7854f7732
syntel <syntel@PHX440G3-2815YV.syntelorg.com>
2018-07-12 15:41:24 -0700

    sdfiaw

--- StoredProcedures/addItems.sql
+++ StoredProcedures/addItems.sql
@@ -1,8 +1,20 @@
 --execute insertItem(useID, fname, veg,item_t, des, timeSlot,pic, pric );
 
-execute insertItem(7, 'delicious food', 'n', 1, 'it is delicious', 0, NULL, 2.3);
+execute insertItem(7, 'Rasam Vada', 'n', 1, 'it is delicious', 0, NULL, 2.3);
 /
-execute insertItem(8, 'delicious food', 'n', 1, 'it is delicious', 0, NULL, 2.3);
+execute insertItem(8, 'Rasam Idly', 'n', 1, 'it is delicious', 0, NULL, 4.3);
 /
-execute insertItem(8, 'delicious food', 'n', 1, 'it is delicious', 0, NULL, 2.3);
+execute insertItem(9, 'Beef Biryani', 'n', 1, 'it is delicious', 0, NULL, 6.3);
+/
+execute insertItem(10, 'Shrimp Biryani ', 'n', 1, 'it is delicious', 0, NULL, 3.3);
+/
+execute insertItem(11, 'Fish Biryani', 'n', 1, 'it is delicious', 0, NULL, 5.3);
+/
+execute insertItem(12, 'Shrimp Biryani', 'n', 1, 'it is delicious', 0, NULL, 3.3);
+/
+execute insertItem(13, 'Pani Poori', 'n', 1, 'it is delicious', 0, NULL, 8.3);
+/
+execute insertItem(13, 'Veg Fried Rice', 'n', 1, 'it is delicious', 0, NULL, 7.3);
+/
+execute insertItem(13, 'Chili Vegetables', 'n', 1, 'it is delicious', 0, NULL, 4.3);
 /
